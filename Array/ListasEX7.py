9f5f915b8e96dea8642183b3a331cf0ba6cdae97
Maykon <mykosistema@gmail.com>
2023-04-14 10:25:58 -0300

    Este commit é objetificado para resolução do sétimo exercício

---
+++ Array/ListasEX7.py
@@ -0,0 +1,11 @@
+numeros = [2, 4, 6, 5, 3]
+
+soma = sum(numeros)
+
+multiplicacao = 1
+for num in numeros:
+    multiplicacao *= num
+
+print("Números Calculados: ", numeros)
+print("Soma: ", soma)
+print("Multiplicação: ", multiplicacao)
