75c6c3e6189aabeb447df8c6352bd3fdc3ca6541
Maykon <mykosistema@gmail.com>
2023-04-14 10:26:33 -0300

    Este commit é objetificado para resolução do nono exercício

---
+++ Array/ListasEX9.py
@@ -0,0 +1,7 @@
+Numeros = [54, 42, 432, 644, 987, 85, 66, 754, 235, 69]
+
+soma_quadrados = 0
+for num in Numeros:
+    soma_quadrados += num ** 2
+
+print(f"A soma dos quadrados dos elementos do vetor A é: {soma_quadrados}")
