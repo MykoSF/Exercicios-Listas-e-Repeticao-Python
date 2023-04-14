1bd3a9f669950c5ea06c1cac27fb6f87368567b9
Maykon <mykosistema@gmail.com>
2023-04-14 14:48:08 -0300

    Este commit é objetificado para manutenção do primeiro exercício

--- Loop/RepetiçãoEX1.py
+++ Loop/RepetiçãoEX1.py
@@ -2,8 +2,8 @@ while True:
     nota = float(input("Digite uma nota entre 0 e 10: "))
     if nota >= 0 and nota <= 10:
         print("Nota válida informada:", nota)
+        break
     else:
         print("Valor inválido, A nota precisa estar entre 0 e 10.")
-        break
 
 
